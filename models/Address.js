a0bd1523eb542eb3789ab81d32dec057ade68ccc
vaishaliigarg <guptavaishi@gmail.com>
2024-02-24 16:15:05 +0530

    Added new routes and updated models for enhanced functionality

--- models/Address.js
+++ models/Address.js
@@ -3,8 +3,7 @@ import mongoose from 'mongoose';
 const addressSchema = new mongoose.Schema({
   user: { type: mongoose.Schema.Types.ObjectId, ref: 'User' },
   name: { type: String },
-  street1: { type: String },
-  street2: { type: String },
+  street: { type: String },
   city: { type: String },
   state: { type: String },
   postalCode: { type: String },
--- models/User.js
+++ models/User.js
@@ -12,9 +12,9 @@ const userSchema = new mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   address: { type: addressSchema },
-  phoneNumber: { type: String },
+  phoneNumber: { type: Number },
 },
 {timestamps:true});
 
-const User = mongoose.model('User', userSchema);
-module.exports = User;
+const User=mongoose.model('User', userSchema);
+export default User;
